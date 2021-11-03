48230ede5e91bd0c961c973e309b7cca37ae5a01
ASePavlova <92172368+ASePavlova@users.noreply.github.com>
2021-11-04 00:55:28 +0200

    Update nums.py

--- nums.py
+++ nums.py
@@ -1,13 +1,14 @@
+#Лабораторная работа 3, список
 def find_missing_nums(nums):
     n = len(nums)
-    s = []
-    b = False
-    for i in range (1,n+1):
-        for j in range (0,n):
-            if i == nums[j]:
-                b = True
+    u = []
+    m = False
+    for p in range (1,n+1):
+        for q in range (0,n):
+            if p == nums[q]:
+                m = True
                 continue
-        if not b:
-            s.append(i)
-        b = False
-    return s
+        if not m:
+            u.append(i)
+        m = False
+    return u
